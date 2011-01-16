39574fc5a6ddd47917e481da92c1bca9497141e2
Chris Cherrett <ccherrett@tsiware.com>
2011-01-16 06:43:07 -0700

    testing fix pc load default

--- oom/midiport.cpp
+++ oom/midiport.cpp
@@ -377,7 +377,7 @@ void MidiPort::sendGmInitValues()
 		// By T356. Initialize from instrument controller if it has an initial value, otherwise use the specified value.
 		// Tested: Ultimately, a track's controller stored values take priority by sending any 'zero time' value
 		//  AFTER these GM/GS/XG init routines are called via initDevices().
-		tryCtrlInitVal(i, CTRL_PROGRAM, 0);
+		//tryCtrlInitVal(i, CTRL_PROGRAM, 0);
 		tryCtrlInitVal(i, CTRL_PITCH, 0);
 		tryCtrlInitVal(i, CTRL_VOLUME, 100);
 		tryCtrlInitVal(i, CTRL_PANPOT, 64);
