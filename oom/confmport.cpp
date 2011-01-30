79ba304953b303ef2bbef4fab96576ec28ada94f
Chris Cherrett <ccherrett@tsiware.com>
2011-01-30 08:11:56 -0700

    un-un-reverted: added the name to the outs in midi port manager

--- oom/confmport.cpp
+++ oom/confmport.cpp
@@ -377,7 +377,6 @@ _redisplay:
 				// Routes can't be re-read until the message sent from msgAddRoute1()
 				//  has had time to be sent and actually affected the routes.
 				///goto _redisplay;   // Go back
-				item->tableWidget()->item(item->row(), DEVCOL_OUTROUTES)->setText(tr("my out"));
 			}
 			delete pup;
 			//iR->setDown(false);     // pup->exec() catches mouse release event
