64bef9da1dab1a25fd3956bb2152f834ae53eb8e
Chris Cherrett <ccherrett@tsiware.com>
2011-01-30 01:08:42 -0700

    small change to the rg conversion script

--- oomfiles/rgd2oom.sh
+++ oomfiles/rgd2oom.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #test $# != 1 && echo "Usage: $0 <rgd-file>" && exit 1
-mkdir -p MUSE_MAPS
+mkdir -p OOM_MAPS
 for f in *.rgd
 do
-	gunzip -c $f | xsltproc $(dirname $0)/rgd2oom.xsl - > "MUSE_MAPS/$(basename $f .rgd).idf"
+	gunzip -c $f | xsltproc $(dirname $0)/rgd2oom.xsl - > "OOM_MAPS/$(basename $f .rgd).idf"
 done
 
