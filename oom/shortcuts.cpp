24dfa58e352b4baa47ac4cdb9397e411db44cedc
alekz49 <compose59@gmail.com>
2011-02-19 21:09:00 +0300

    adjust shortcuts

--- oom/shortcuts.cpp
+++ oom/shortcuts.cpp
@@ -170,8 +170,8 @@ void initShortCuts()
 	defShrt(SHRT_LOCATORS_TO_SELECTION, Qt::ALT + Qt::Key_P, "Edit: Set locators to selection", ARRANG_SHRT + PROLL_SHRT + DEDIT_SHRT, "loc_to_sel");
 	defShrt(SHRT_INC_PITCH, Qt::CTRL + Qt::Key_Up, "Edit: Increase pitch", PROLL_SHRT + DEDIT_SHRT, "sel_inc_pitch");
 	defShrt(SHRT_DEC_PITCH, Qt::CTRL + Qt::Key_Down, "Edit: Decrease pitch", PROLL_SHRT + DEDIT_SHRT, "sel_dec_pitch");
-	defShrt(SHRT_INC_POS, Qt::CTRL + Qt::Key_period, "Edit: Increase event position", PROLL_SHRT + DEDIT_SHRT, "sel_inc_pos");
-	defShrt(SHRT_DEC_POS, Qt::CTRL + Qt::Key_comma, "Edit: Decrease event position", PROLL_SHRT + DEDIT_SHRT, "sel_dec_pos");
+	defShrt(SHRT_INC_POS, Qt::CTRL + Qt::Key_Period, "Edit: Increase event position", PROLL_SHRT + DEDIT_SHRT, "sel_inc_pos");
+	defShrt(SHRT_DEC_POS, Qt::CTRL + Qt::Key_Comma, "Edit: Decrease event position", PROLL_SHRT + DEDIT_SHRT, "sel_dec_pos");
 	defShrt(SHRT_ZOOM_IN, Qt::CTRL + Qt::Key_PageUp, "View: Zoom in", PROLL_SHRT + DEDIT_SHRT, "zoom_in");
 	defShrt(SHRT_ZOOM_OUT, Qt::CTRL + Qt::Key_PageDown, "View: Zoom out", PROLL_SHRT + DEDIT_SHRT, "zoom_out");
 	defShrt(SHRT_GOTO_CPOS, Qt::CTRL + Qt::Key_J, "View: Goto Current Position", PROLL_SHRT + DEDIT_SHRT, "goto_cpos");
