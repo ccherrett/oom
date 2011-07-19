b09ea8be4b8a54d6e7dda209369972aba89e0afa
Andrew Williams <linuxras@gmail.com>
2011-07-19 01:07:14 -0500

    Adding the tracklistview files

---
+++ oom/widgets/tracklistview.cpp
@@ -0,0 +1,143 @@
+//===========================================================
+//  OOMidi
+//  OpenOctave Midi and Audio Editor
+//  (C) Copyright 2011 Andrew Williams & Christopher Cherrett
+//===========================================================
+
+#include <QtGui>
+#include "tracklistview.h"
+#include "globals.h"
+#include "gconfig.h"
+#include "app.h"
+#include "song.h"
+#include "track.h"
+#include "part.h"
+#include "midieditor.h"
+
+TrackListView::TrackListView(MidiEditor* editor, QWidget* parent)
+: QFrame(parent)
+{
+	m_editor = editor;
+	m_layout = new QVBoxLayout(this);
+	m_layout->setContentsMargins(8, 2, 8, 2);
+	m_model = new QStandardItemModel(0, 2, this);
+	m_table = new QTableView(this);
+	m_table->setObjectName("TrackListView");
+	m_table->setModel(m_model);
+	m_table->setAlternatingRowColors(true);
+	m_table->setSelectionMode(QAbstractItemView::SingleSelection);
+	m_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+	m_table->setCornerButtonEnabled(false);
+	m_table->horizontalHeader()->setStretchLastSection(true);
+	m_table->verticalHeader()->hide();
+	m_layout->addWidget(m_table);
+	songChanged(-1);
+	connect(song, SIGNAL(songChanged(int)), this, SLOT(songChanged(int)));
+	connect(m_model, SIGNAL(itemChanged(QStandardItem*)), this, SLOT(toggleTrackPart(QStandardItem*)));
+}
+
+TrackListView::~TrackListView()
+{
+}
+
+void TrackListView::songChanged(int flags)/*{{{*/
+{
+	if(flags == -1 || flags & (SC_TRACK_INSERTED | SC_TRACK_REMOVED | SC_PART_INSERTED | SC_PART_REMOVED))
+	{
+		m_model->clear();
+		for(iMidiTrack i = song->midis()->begin(); i != song->midis()->end(); ++i)
+		{
+			MidiTrack* track = (MidiTrack*)(*i);
+			PartList* pl = track->parts();
+			QList<QStandardItem*> trackRow;
+			QStandardItem* chkTrack = new QStandardItem(true);
+			chkTrack->setCheckable(true);
+			chkTrack->setData(1, TrackRole);
+			chkTrack->setData(track->name(), TrackNameRole);
+			trackRow.append(chkTrack);
+			QStandardItem* trackName = new QStandardItem();
+			trackName->setText(track->name());
+			QFont font = trackName->font();
+			font.setBold(true);
+			trackName->setFont(font);
+			trackRow.append(trackName);
+			m_model->appendRow(trackRow);
+			for(iPart ip = pl->begin(); ip != pl->end(); ++ip)
+			{
+				QList<QStandardItem*> partsRow;
+				Part* part = ip->second;
+				QStandardItem* chkPart = new QStandardItem(true);
+				chkPart->setCheckable(true);
+				chkPart->setData(part->sn(), PartRole);
+				chkPart->setData(2, TrackRole);
+				chkPart->setData(track->name(), TrackNameRole);
+				chkPart->setData(part->tick(), TickRole);
+				if(m_editor->hasPart(part->sn()))
+				{
+					chkPart->setCheckState(Qt::Checked);
+				}
+				QStandardItem* partName = new QStandardItem(part->name());
+				partsRow.append(chkPart);
+				partsRow.append(partName);
+				m_model->appendRow(partsRow);
+			}
+		}
+		m_table->setColumnWidth(0, 20);
+	}
+}/*}}}*/
+
+void TrackListView::toggleTrackPart(QStandardItem* item)
+{
+	int type = item->data(TrackRole).toInt();
+	bool checked = (item->checkState() == Qt::Checked);
+	QString trackName = item->data(TrackNameRole).toString();
+	Track* track = song->findTrack(trackName);
+	if(!track || !m_editor)
+		return;
+
+	PartList* list = track->parts();
+	if(list->empty())
+		return;
+	switch(type)
+	{
+		case 1: //Track
+		{
+			for(iPart i = list->begin(); i != list->end(); ++i)
+			{
+				if(checked)
+				{
+					if(!m_editor->hasPart(i->second->sn()))
+						m_editor->addPart(i->second);
+				}
+				else
+				{
+					if(m_editor->hasPart(i->second->sn()))
+					{
+						m_editor->removePart(i->second->sn());
+					}
+				}
+			}
+			if(!list->empty())
+				songChanged(-1);
+		}
+		break;
+		case 2: //Part
+		{
+			int sn = item->data(PartRole).toInt();
+			unsigned tick = item->data(TickRole).toInt();
+			Part* part = list->find(tick, sn);
+			if(part)
+			{
+				if(checked)
+					m_editor->addPart(part);
+				else
+					m_editor->removePart(sn);
+			}
+		}
+		break;
+	}
+}
+
+void TrackListView::removePart(unsigned, QString)
+{
+}
---
+++ oom/widgets/tracklistview.h
@@ -0,0 +1,41 @@
+//===========================================================
+//  OOMidi
+//  OpenOctave Midi and Audio Editor
+//  (C) Copyright 2011 Andrew Williams & Christopher Cherrett
+//===========================================================
+
+#include <QFrame>
+#define PartRole Qt::UserRole+2
+#define TrackRole Qt::UserRole+3
+#define TrackNameRole Qt::UserRole+4
+#define TickRole Qt::UserRole+5
+
+class QTableView;
+class QStandardItem;
+class QStandardItemModel;
+class QItemSelectionModel;
+class QString;
+class QVBoxLayout;
+
+class Part;
+class Track;
+class MidiEditor;
+
+class TrackListView : public QFrame
+{
+	Q_OBJECT
+
+	QTableView* m_table;
+	QStandardItemModel* m_model;
+	QVBoxLayout* m_layout;
+	MidiEditor* m_editor;
+
+private slots:
+	void songChanged(int);
+public slots:
+	void removePart(unsigned tick, QString name);
+	void toggleTrackPart(QStandardItem*);
+public:
+	TrackListView(MidiEditor* e, QWidget* parent = 0);
+	~TrackListView();
+};
