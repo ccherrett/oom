8300886115d45944730762b423982f26ca1c61c9
Andrew Williams <linuxras@gmail.com>
2011-10-04 19:51:58 -0500

    Added distro syncing script to oomfiles

---
+++ oomfiles/makedist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+DESTDIR=~/music/OOMidi-release/
+OOMBASE="oom-"
+SRCDIR=~/workspace/git-projects/oom/
+OOMEXCLUDES=${SRCDIR}oomfiles/excludes
+VERSION=${1}
+CMD=${DESTDIR}${OOMBASE}${VERSION}/
+if [ -z "$VERSION" ]; then
+	echo "Usage: $0 <version>"
+	echo "Example: $0 2011.3"
+else
+	mkdir -p "${DESTDIR}"
+	echo "Syncing distro"
+	rsync -av --progress --delete-excluded --exclude-from=${OOMEXCLUDES} ${SRCDIR} ${CMD}
+fi
