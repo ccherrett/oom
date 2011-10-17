78c028ac65eb3d0449d84db930e8aa9a79c32e39
Andrew Williams <linuxras@gmail.com>
2011-10-17 14:14:54 -0500

    OOStudio: Updated sourceforge url in download xml

--- oostudio/OOThread.h
+++ oostudio/OOThread.h
@@ -10,23 +10,60 @@
 #include <QThread>
 
 class QProcess;
+struct LogInfo;
 
 class OOThread : public QThread
 {
 	Q_OBJECT
 
+protected:
 	QProcess* m_process;
 
 public:
-	OOThread(QProcess* p, QObject* parent = 0);
+	OOThread(QObject* parent = 0);
 	void run();
 
-public slot:
+public slots:
 	bool startJob();
 
 signals:
 	void startupSuccess();
 	void startupFailed();
+	void logMessage(LogInfo*);
 };
 
+class OOMProcessThread : public OOThread
+{
+	Q_OBJECT
+public:
+	OOMProcessThread(QObject* parent = 0);
+	void run();
+
+public slots:
+	virtual bool startJob();
+
+};
+
+class JackProcessThread : public QThread
+{
+	Q_OBJECT
+public:
+	JackProcessThread(QObject* parent = 0);
+	void run();
+
+public slots:
+	virtual bool startJob();
+};
+
+class LinuxSamplerThread : public QThread
+{
+	Q_OBJECT
+
+public:
+	LinuxSamplerThread(QObject* parent = 0);
+	void run();
+
+public slot:
+	virtual bool startJob();
+};
 #endif
