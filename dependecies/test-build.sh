f5564236e5e8a9b00d589a87907518246830c333
falkTX <falktx@gmail.com>
2011-11-02 16:25:48 +0000

    lilv static build test

---
+++ dependecies/test-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SERD_VERSION=0.5.0
+SORD_VERSION=0.5.0
+LILV_VERSION=0.5.0
+
+export PKG_CONFIG_PATH=`pwd`/build/lib/pkgconfig:$PKG_CONFIG_PATH
+
+rm -rf build *.h *.hpp *.a
+mkdir build
+
+# Compile serd
+cd serd-$SERD_VERSION
+# patch -p1 -N < ../serd-dont_run_ldconfig.patch
+./waf configure --prefix=../build --no-utils --static
+./waf build
+./waf install
+cd ..
+
+# Compile sord
+cd sord-$SORD_VERSION
+# patch -p1 -N < ../sord-dont_run_ldconfig.patch
+./waf configure  --prefix=../build --static
+./waf build
+./waf install
+cd ..
+
+# Compile lilv
+cd lilv-$LILV_VERSION
+./waf configure  --prefix=../build --static
+./waf build
+cp build/*.a lilv/lilv.h lilv/lilvmm.hpp ..
+cd ..
