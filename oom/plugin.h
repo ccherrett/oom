f25df2ae5bff5f07052e042cb1d7ef06a4b23ce5
falkTX <falktx@gmail.com>
2012-01-20 00:45:21 +0000

    Fix GridLayout issue on gensetbase.ui file

--- oom/plugin.h
+++ oom/plugin.h
@@ -57,7 +57,7 @@ class Xml;
 
 // plugin hints
 const unsigned int PLUGIN_IS_SYNTH            = 0x01;
-const unsigned int PLUGIN_IS_FX               = 0x02; // requires at least 1 audio port in and out
+const unsigned int PLUGIN_IS_FX               = 0x02;
 const unsigned int PLUGIN_HAS_NATIVE_GUI      = 0x04;
 const unsigned int PLUGIN_HAS_IN_PLACE_BROKEN = 0x08;
 
