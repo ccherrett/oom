5144b1bfd523ff49253ddf11c3b7f723ece03ecb
Andrew Williams <linuxras@gmail.com>
2012-02-21 09:25:21 -0500

    Fixed for controller lane

--- oom/midiedit/Performer.cpp
+++ oom/midiedit/Performer.cpp
@@ -2177,6 +2177,9 @@ void Performer::updateControllerForInstrument(qint64 trackId)/*{{{*/
 	}
 	if(setDefaults)
 	{
+		saveInstrumentControllerState();
+		while(ctrlEditList.size())
+			delete ctrlEditList.takeFirst();
 		QStringList lctrl = DEFAULT_CONTROLLERS.split("::", QString::SkipEmptyParts);
 		foreach(QString ctl, lctrl)
 		{
