0c2187a1f38777694e98b5eac9819e27924ca3bd
falkTX <falktx@gmail.com>
2012-02-28 16:11:05 +0000

    Continue work on rubberband, still unfinished

--- oom/widgets/StretchDialog.h
+++ oom/widgets/StretchDialog.h
@@ -9,6 +9,7 @@
 #define __STRETCH_DIALOG_H__
 
 #include <QDialog>
+#include <QProcess>
 
 namespace Ui {
 class StretchDialog;
@@ -22,9 +23,19 @@ public:
     StretchDialog(QWidget* parent = 0);
     ~StretchDialog();
     
+    void setFile(QString filename, QString dirName);
+    //QString getNewFilename();
+
+public Q_SLOTS:
+    virtual void accept();
+
 protected:
     Ui::StretchDialog* ui;
     
+private:
+    QString m_filename;
+    QString m_dirName;
+    QProcess m_proc;
 };
 
 #endif // __STRETCH_DIALOG_H__
