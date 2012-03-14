c9bcdd14675849d3053a24cc6339229b165ad0bc
Andrew Williams <linuxras@gmail.com>
2012-03-14 15:07:40 -0500

    More debug, trying to fix enless loop

--- oom/TrackManager.cpp
+++ oom/TrackManager.cpp
@@ -840,8 +840,7 @@ void TrackManager::setTrackInstrument(qint64 tid, const QString& instrument, int
 						if (synth->duplicated())
 						{
 							midiDevices.remove(md);
-							synth->close();
-							delete synth;
+							//synth->close();
 						}
 						//if(debugMsg)
 							qDebug("TrackManager::setTrackInstrument: Synth cleanup complete");
@@ -863,7 +862,7 @@ void TrackManager::setTrackInstrument(qint64 tid, const QString& instrument, int
 							QString postfix("-audio");
 							QString devname(QString(prefix).append(m_track->name()));
 							QString audioName(QString(prefix).append(m_track->name()).append(postfix));
-							QString midi(QString("O-").append(m_track->name()));
+							QString midi(QString("O").append(m_track->name()));
 
 							MidiDevice *ndev = MidiJackDevice::createJackMidiDevice(midi, 3);
 							if(ndev)
